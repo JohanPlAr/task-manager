976df51f241c2866fa816198473a62a8d206a11d
JohanPlAr <>
2023-10-30 12:32:54 +0000

    Add postgres Database to __init__.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -3,12 +3,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env  # noqa
-
+from taskmanager import routes  # noqa
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+if os.environ.get("DEVELOPMENT")=="True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+        db = SQLAlchemy(app)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
-db = SQLAlchemy(app)
-
-from taskmanager import routes  # noqa
